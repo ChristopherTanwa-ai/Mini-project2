0706e116af1464d50bd49c9f5d405fda8c0b45fd
ChristopherTanwa-ai <clethjorgensen@gmail.com>
2023-03-27 14:24:13 +0200

    alt muligt

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const data = require('./data.json');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+
+app.set('view engine', 'ejs')
+app.set('views',path.join(__dirname,'/views'))
+
+app.get('/', (req, res) => {
+    console.log("we got a request")
+    res.render('home')
+})
+
+app.get('/random', (req,res) =>{
+    const num =  Math.floor(Math.random() *10) + 1;
+    res.render('random', {rand: num})
+})
+
+// Path parameter
+app.get('/:product',(req,res) => {
+    console.log(req.params)
+    const product = req.params;
+    res.render('product',{name: product} )
+})
+
+app.get('/dog', (req, res) => {
+    console.log("we got a dog request")
+    res.send("Doogo")
+})
+
+
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
