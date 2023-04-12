13ecfae329507143a3e2232102b52f3bb5782fee
“Jakob” <jaken@itu.dk>
2023-04-12 11:53:30 +0200

    fjernet_redundans

--- script.js
+++
@@ -1,8 +0,0 @@
-function addToCart(input) {
-    let foundUser = users.find((data) => session.userid === data.Username);
-    const posters = mainData.Posters;
-    let index = poster.get(input)
-    const poster = posters[index];
-    foundUser.basket.push(poster)
-    console.log('User list', users);
-}
